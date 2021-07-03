2212812fc07eb1ac49aa3e84cf616eba5e669388
Cydney Pollard <sincerelycydney087@gmail.com>
2021-07-02 22:11:45 -0400

    added client react app and created components

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import react, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        Hello World!
+      </div>
+    )
+  }
+};
